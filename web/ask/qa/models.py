689b89d443ef3e612505e7346832bdded8701c8b
Abc <roma.baranov@gmail.com>
2020-05-14 13:28:15 +0300

    Classes Question and Answer added

--- web/ask/qa/models.py
+++ web/ask/qa/models.py
@@ -1,3 +1,26 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=255)
+    text = models.TextField()
+    added_at = models.DateField()
+    rating = models.IntegerField()
+    author = User()
+    likes = User()
+
+
+class QuestionManager(models.Manager):
+    def new(self):
+        self.order
+
+    def popular(self):
+        pass
+
+
+class Answer(models.Model):
+    text = models.TextField()
+    added_at = models.DateField()
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    author = User()
